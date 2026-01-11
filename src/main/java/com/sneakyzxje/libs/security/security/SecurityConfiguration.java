6d52dce325c4c523210c6d2766b66cdccc0a9f30
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-12 00:34:24 +0700

    feat(security): enable method security and add jwt access denied handler

--- src/main/java/com/sneakyzxje/libs/security/security/SecurityConfiguration.java
+++ src/main/java/com/sneakyzxje/libs/security/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.sneakyzxje.libs.security.security;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.sneakyzxje.libs.security.filter.JwtFilter;
 import com.sneakyzxje.libs.security.properties.SecurityProperties;
+import com.sneakyzxje.libs.security.security.exception.JwtAccessDeniedHandler;
 import com.sneakyzxje.libs.security.security.exception.JwtEntryPoint;
 
 import lombok.RequiredArgsConstructor;
@@ -19,10 +21,12 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(SecurityProperties.class)
+@EnableMethodSecurity
 public class SecurityConfiguration {
     private final SecurityProperties securityProperties;
     private final JwtFilter jwtFilter;
     private final JwtEntryPoint jwtEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         String[] publicEndpoints = securityProperties.getEndpoints().toArray(new String[0]);
@@ -34,6 +38,7 @@ public class SecurityConfiguration {
             .anyRequest().authenticated()
         )
         .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtEntryPoint))
+        .exceptionHandling(exception -> exception.accessDeniedHandler(jwtAccessDeniedHandler))
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
